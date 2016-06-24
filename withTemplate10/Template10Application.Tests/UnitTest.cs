95aa11091bbcc2a77e2a19d5931ec1ed8e3ee3c9
Michael Sarchet <misarche@microsoft.com>
2016-06-24 11:24:54 -0700

    Validate test cases

---
+++ withTemplate10/Template10Application.Tests/UnitTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework.AppContainer;
+
+namespace Template10Application.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [UITestMethod]
+        public void TestMethod1()
+        {
+            var testControl = new TestControl();
+            testControl.Text = "Yay";
+            Microsoft.VisualStudio.TestPlatform.UnitTestFramework.Assert.AreEqual(testControl.Text, "Yay");
+        }
+    }
+}
---
+++ withTemplate10/Template10Application/TestControl.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace Template10Application
+{
+    public sealed partial class TestControl : UserControl
+    {
+        public string Text
+        {
+            get; set;
+        }
+
+        public static readonly DependencyProperty TextProperty =
+            DependencyProperty.Register(nameof(Text), typeof(string), typeof(TestControl), new PropertyMetadata(null));
+        public TestControl()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
